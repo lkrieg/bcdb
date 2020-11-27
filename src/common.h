ce79e5368033f7180f142faef9fdc3b10b6404d0
Leon Krieg <info@madcow.dev>
2020-11-27 16:32:31 +0100

    Add filesystem implementation file

--- src/common.h
+++ src/common.h
@@ -6,34 +6,48 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#define  DEBUG                          // Enable debug mode
-#define  DEFAULT_PORT     23            // Default server socket port
-#define  MAX_MSG_LEN      2048          // Maximum log message length
-#define  MAX_HASH_SIZE    4096          // Maximum number of hash buckets
-#define  MAX_PATH         4096          // Cannot rely on system header
+//       Constants
+#define  DEBUG                           // Enable debug mode
+#define  DEFAULT_PORT      23            // Default server socket port
+#define  MAX_MSG_LEN       2048          // Maximum log message length
+#define  MAX_HASH_SIZE     4096          // Maximum number of hash buckets
+#define  MAX_PATH          4096          // Cannot rely on system header
 
-#define  E_ASSERT         "Assertion failure"
-#define  E_NOMEM          "Memory allocation failure"
-#define  E_NOSOCK         "Could not bind to socket"
+//       Messages
+#define  E_ASSERT          "Assertion failure"
+#define  E_NOMEM           "Memory allocation failure"
+#define  E_NOSOCK          "Could not bind to socket"
 
-#define  BIT(n)           (1UL << (n))  // Get bitmask for nth bit
-#define  UNUSED(sym)      ((void)(sym)) // Suppress -Wunused warnings
-#define  Allocate(n)      _Allocate(n)  // Replacement for malloc()
-#define  Free(ptr)        _Free(ptr)    // Replacement for free()
-#define  Assert(exp)      UNUSED(0)     // Check runtime assertion
-#define  Verbose(...)     UNUSED(0)     // Output debug info message
+//       Utility
+#define  BIT(n)            (1UL << (n))  // Get bitmask for nth bit
+#define  UNUSED(sym)       ((void)(sym)) // Suppress -Wunused warnings
+#define  Assert(exp)       UNUSED(0)     // Check runtime assertion
 
-void     Info(const char *fmt, ...);    // Output general info message
-void     Error(const char *fmt, ...);   // Handle non-recoverable failure
+//       Memory
+#define  Allocate(n)       _Allocate(n)  // Replacement for malloc()
+#define  Free(ptr)         _Free(ptr)    // Replacement for free()
 
-typedef  uint32_t         u32;          // Guaranteed 32 bit data type
-typedef  uint16_t         u16;          // Guarenteed 16 bit data type
-typedef  unsigned char    byte;         // uint8_t could be a non-character
-                                        //   type and break strict aliasing
-#ifdef   DEBUG                          // DEBUG mode has multiple effects:
-#undef   Allocate                       // - Tracks all memory allocations
-#undef   Verbose                        // - Outputs verbose log messages
-#undef   Assert                         // - Enables runtime assertions
+//       Logging
+void     Info (const char  *fmt, ...);   // Output general info message
+void     Error(const char  *fmt, ...);   // Handle non-recoverable failure
+#define  Verbose(...)      UNUSED(0)     // Output debug info message
+
+//       Filesystem
+int      FS_Open(const char *path);
+int      FS_Read(int fd, void *dest, int n);
+int      FS_Write(int fd, const void *src, int n);
+void     FS_Close(int handle);
+
+//       Typedefs
+typedef  uint32_t          u32;          // Guaranteed 32 bit data type
+typedef  uint16_t          u16;          // Guarenteed 16 bit data type
+typedef  unsigned char     byte;         // uint8_t could be a non-character
+                                         //   type and break strict aliasing
+//       Debug
+#ifdef   DEBUG                           // DEBUG mode has multiple effects:
+#undef   Allocate                        // - Tracks all memory allocations
+#undef   Verbose                         // - Outputs verbose log messages
+#undef   Assert                          // - Enables runtime assertions
 #undef   Free
 
 #define  Verbose(...)     _Verbose(__VA_ARGS__)
---
+++ src/common/filesystem.c
@@ -0,0 +1,28 @@
+#include "common.h"
+
+int FS_Open(const char *path)
+{
+	UNUSED(path);
+	return -1;
+}
+
+int FS_Read(int fd, void *dest, int n)
+{
+	UNUSED(fd);
+	UNUSED(dest);
+	UNUSED(n);
+	return -1;
+}
+
+int FS_Write(int fd, const void *src, int n)
+{
+	UNUSED(fd);
+	UNUSED(src);
+	UNUSED(n);
+	return -1;
+}
+
+void FS_Close(int handle)
+{
+	UNUSED(handle);
+}
