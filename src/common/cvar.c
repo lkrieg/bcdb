24a6035d5f585d70ca0d635d2504894fa73f74d2
Leon Krieg <madcow@mailbox.org>
2020-12-11 22:14:28 +0100

    Statically allocate memory for cvars

--- src/common/cvar.c
+++ src/common/cvar.c
@@ -1,24 +1,28 @@
 #include "common.h"
 #include "cvar.h"
 
-typedef union {
-         char *  str;
-         int     num;
-} cvar;
+static union {
+	char *  str;
+	int     num;
+} cvars[NUM_CVARS];
 
 int CFG_Init(void)
 {
+	// TODO
+	UNUSED(cvars);
 	return 0;
 }
 
 int CFG_GetNumber(int type)
 {
+	// TODO
 	UNUSED(type);
 	return 0;
 }
 
 char *CFG_GetString(int type)
 {
+	// TODO
 	UNUSED(type);
 	return NULL;
 }
