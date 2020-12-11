bf60eed2296e79edb546a9bd331792ee6dfdf8b9
Leon Krieg <madcow@mailbox.org>
2020-12-11 20:28:30 +0100

    Handle command-line arguments

--- src/common/cmdarg.c
+++ src/common/cmdarg.c
@@ -1,28 +1,62 @@
 #include "common.h"
 #include "cmdarg.h"
-#include <unistd.h>
+#include <getopt.h>
+#include <string.h>
 
-static int    argnum;
+static int    argnum, index;
 static arg_t  argbuf[MAX_ARG_NUM];
 
 void CMD_Init(int argc, char **argv)
 {
+	arg_t *head;
+	int c, i = 0;
+	static char opstr[] = "dkhf:";
+	static struct option opts[] = {
+		{"daemon",  no_argument,        0,  'd'},
+		{"kill",    no_argument,        0,  'k'},
+		{"help",    no_argument,        0,  'h'},
+		{"file",    optional_argument,  0,  'f'},
+		{0,         0,                  0,   0 }};
+
 	if (argc > MAX_ARG_NUM)
 		Error(E_ARGNUM);
 
-	UNUSED(argc);
-	UNUSED(argv);
-	UNUSED(argnum);
-	UNUSED(argbuf);
+	opterr = 0;
+	while ((c = getopt_long(argc, argv, opstr, opts, &i)) >= 0) {
+		head = &argbuf[argnum++];
+		switch (c) {
+		case 'd':  head->type = T_ARG_FORK; break;
+		case 'k':  head->type = T_ARG_KILL; break;
+		case 'h':  head->type = T_ARG_HELP; break;
+		case -1 :  head->type = T_ARG_NONE; return;
+		case 'f':  if (optarg != NULL) {
+				if (strlen(optarg) >= MAX_ARG_LEN)
+					Error(E_ARGLEN);
+
+				head->type = T_ARG_FILE;
+				strcpy(head->value, optarg);
+				break;
+		           } // fallthrough
+		default:
+		case ':':
+		case '?':  argnum++;
+			   argbuf[0].type = T_ARG_INVALID;
+		           return;
+		}
+	}
 }
 
 int CMD_Next(arg_t *out)
 {
-	// T_ARG_INVALID
-	// T_ARG_DAEMON
-	// T_ARG_SHUTDOWN
-	// T_ARG_IMPORT
+	arg_t *head;
 
-	out->type = T_ARG_INVALID;
+	if (index >= argnum) {
+		out->type = T_ARG_NONE;
+		return out->type;
+	}
+
+	head = &argbuf[index++];
+	*out = *head;
+
 	return out->type;
 }
--- src/common/errors.h
+++ src/common/errors.h
@@ -4,5 +4,6 @@
 #define E_ASSERT  "Assertion failure"
 #define E_NOMEM   "Memory allocation failure"
 #define E_ARGNUM  "Too many command-line arguments"
+#define E_ARGLEN  "Command-line argument too long"
 
 #endif // ERRORS_H
