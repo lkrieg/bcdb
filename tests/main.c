38eb708935db3269ed58c1f4c929555170a8537f
Leon Krieg <madcow@mailbox.org>
2020-12-17 23:27:45 +0100

    Temporary fix for filesystem dependency throwing off CI

--- tests/main.c
+++ tests/main.c
@@ -4,6 +4,10 @@
 // See all source files prefixed with "test-" for the actual tests.
 // Implementation of unit testing fixtures can be found in util.h.
 
+// TODO: Remove filesystem dependencies
 // TODO: Compile test seperatly
+
 #include "database/filesystem.c"
+#include "database/database.c"
+#include "database/hashtable.c"
 #include "util.c"
