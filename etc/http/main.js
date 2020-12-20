7bebe4167aaeb4be42048773d9fc60154829cad1
Leon Krieg <leonkrieg@mailbox.org>
2020-12-20 09:28:48 +0100

    Implement more efficient http client polling using database timestamp

--- etc/http/main.js
+++ etc/http/main.js
@@ -1,12 +1,56 @@
-function rowStyle(row, index) {
-  var status = [
-    "waiting",
-    "scanned",
-    "unknown",
-    "invalid"
-  ];
+var time; // last database update
 
-  return {
-    classes: status[row.status]
-  };
+function rowStyle(row, index)
+{
+	var status = [
+		"waiting",
+		"scanned",
+		"unknown",
+		"invalid"
+	];
+
+	return {
+		classes: status[row.status]
+	};
 }
+
+function poll(url, callback)
+{
+	var xhr = new XMLHttpRequest();
+
+	xhr.open("GET", url, true);
+	xhr.onload = function (e) {
+		if ((xhr.readyState === 4)
+		&& ((xhr.status === 200 && xhr.response.length > 0)))
+			callback(xhr.responseText);
+	};
+
+	xhr.send(null);
+}
+
+function update()
+{
+	var newtime;
+
+	poll("/time", function(data) {
+		setTimeout(update, 1000);
+		newtime = parseInt(data);
+		if (newtime > time) {
+			time = newtime;
+			refreshTable();
+		}
+	});
+}
+
+function refreshTable()
+{
+	$('#table').bootstrapTable('refresh', {silent: true});
+}
+
+function main()
+{
+	time = 1;
+	setTimeout(update, 500);
+}
+
+window.onload = main;
